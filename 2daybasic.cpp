1fb489d31abbb01b61027735f841f57ee8030139
abhishekallipur <abhishek69allipur@gmail.com>
2025-09-26 23:18:52 +0530

    break and pointer basic

--- 2daybasic.cpp
+++ 2daybasic.cpp
@@ -6,10 +6,16 @@ int main()
     {
         if (i == 2)
         {
-               continue;
+               continue;//break and continue
             cout << "hello world" <<i<<endl;
-        }   
+        }
         cout << "hi abhishek" <<i<<endl;
     }
+    // this pointer
+    
+    int a = 10;//this int 
+    int *b = &a;//where b store the address of the a 
+    cout << "this address of the a " << b << endl;//this print the address of a 
+    cout << "this is actual value of " << *b;//this * is used for print thr actual value of a 
     return 0;
-}
+}   
